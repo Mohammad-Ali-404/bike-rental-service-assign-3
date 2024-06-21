c3ed8da07633ad0379115fa257c6a26e95d90c1b
wpmohammad1 <alimohammadjhd@gmail.com>
2024-06-21 12:10:59 +0600

    run sever

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from "express";
+
+import cors from "cors";
+
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+//Application
+// app.use("/api/products", ProductRoutes);
+// app.use("/api/orders", OrdersRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+export default app;
